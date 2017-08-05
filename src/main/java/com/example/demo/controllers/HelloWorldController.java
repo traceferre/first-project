8a24904baa5ea3310b7c0978a4e655c3b6403f95
Curtis Schlak <curtis.schlak@theironyard.com>
2017-08-05 11:26:42 -0500

    Initial commit.

---
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping({"/", "/HelloWorld"})
+public class HelloWorldController {
+	
+	private String title;
+	
+	public HelloWorldController() {
+		title = "Hello Java and Spring!";
+	}
+	
+	@GetMapping("")
+	public String index() {
+		return "helloworld/index";
+	}
+	
+	@GetMapping("message")
+	public ModelAndView message(@RequestParam(required=false, defaultValue="«silence»") String message) {
+		ModelAndView mv = new ModelAndView("helloworld/message");
+		mv.addObject("title", title);
+		mv.addObject("message", message);
+		return mv;
+	}
+	 
+	@PostMapping("adder")
+	public String addTwoNumbers(@RequestParam(name="left") int first, @RequestParam(name="right") double second, Model model) {
+		model.addAttribute("sum", first + second);
+		return "helloworld/sum-result";
+	}
+	
+}
